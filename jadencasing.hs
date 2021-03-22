4571d4ecdbbb03543275e96126998f43e2ade89d
Chaitanya Dandugula <chaitanya@chadan.in>
2021-03-22 15:44:20 +0530

    +1 Codewars solution

---
+++ jadencasing.hs
@@ -0,0 +1,12 @@
+module JadenCasing where
+import qualified Data.Char as Char
+
+capitalizeFirstChar :: String -> String
+capitalizeFirstChar [] = []
+capitalizeFirstChar (head:tail) = Char.toUpper head : lowered tail
+  where
+    lowered [] = []
+    lowered(head:tail) = Char.toLower head : lowered tail
+
+toJadenCase :: String -> String
+toJadenCase js = unwords $ map capitalizeFirstChar $ words js
