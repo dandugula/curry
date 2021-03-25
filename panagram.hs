b58d6d4d0c21a2f9ec4c0893c0198b7719d5bcb8
Chaitanya Dandugula <chaitanya.dandugula@spirent.com>
2021-03-25 17:28:54 +0530

    Using . instead of $

--- panagram.hs
+++ panagram.hs
@@ -3,4 +3,4 @@ import Data.List
 import Data.Char
 
 isPangram :: String -> Bool
-isPangram str = ['a'..'z'] == (sort $ nub $ map toLower $ filter isAlpha str)
+isPangram str = ['a'..'z'] == (sort . nub . map toLower $ filter isAlpha str)
